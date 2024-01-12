2d9b66900126f60f59238cda588f34cd822b91b3
m0ts <markandtorres@gmail.com>
2024-01-12 09:34:52 -0500

    answer to problem 3

--- index.js
+++ index.js
@@ -15,14 +15,24 @@ function addNewStore(stores, store) {
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  if (index >= 0 && index < stores.length) {
+    stores.splice(index, 1);
+  }
+  return stores;
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+  const newStore = {...store}; 
+  newStore.boardGames = [...newStore.boardGames];
+  newStore.address = {...newStore.address};
+  return newStore;
+}
 
 module.exports = {
   addNewStore,
